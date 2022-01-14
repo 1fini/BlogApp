10cbdfd2c1f8a7b0bc1dccfa7c9b7b0057fba331
Dan GIORDANI <hinomura.yo@gmail.com>
2022-01-14 21:45:42 +0100

    Initial Commit

---
+++ Shared/BlogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogAppApp.swift
+//  Shared
+//
+//  Created by Dan on 14/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BlogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
