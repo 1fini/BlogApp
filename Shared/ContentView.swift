38faf8d693090ce28b113261346afd85d63b0977
Dan GIORDANI <hinomura.yo@gmail.com>
2022-01-14 23:36:33 +0100

    init

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var  results = [Post]()
+    
     var body: some View {
         Text("Hello, world!")
-            .padding()
+        List(results, id: \.id) {
+            item in
+            VStack(alignment: .leading){
+                Text(item.title)
+                    .font(.headline)
+                Text(item.slug)
+            }
+        }.task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://www.parentsimpliques.fr/wp-json/wp/v2/posts") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            // more code to come
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.articles
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
@@ -18,4 +47,17 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Response : Codable {
+    var articles: [Post]
+}
+
+struct Post : Codable {
+    var id : Int
+    var date : Date
+    var guid : String
+    var slug : String
+    var link : URL
+    var title : String
 }
---
+++ Shared/PostContent.swift
@@ -0,0 +1,8 @@
+//
+//  PostContent.swift
+//  BlogApp
+//
+//  Created by Dan on 14/01/2022.
+//
+
+import Foundation
