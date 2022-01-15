2f5b4fbbcf1906308b961a8107827b79e48ec68e
Dan GIORDANI <hinomura.yo@gmail.com>
2022-01-15 15:57:47 +0100

    Display blog posts title and id

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,34 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var  results = [Post]()
+    @State private var results = [Post]()
     
     var body: some View {
         Text("Hello, world!")
         List(results, id: \.id) {
             item in
             VStack(alignment: .leading){
-                Text(item.title)
+                
+                Text(item.title.rendered)
                     .font(.headline)
-                Text(item.slug)
+                Text(String.init(item.id))
             }
-        }.task {
+        }
+        .task {
             await loadData()
         }
     }
     
     func loadData() async {
+        
         guard let url = URL(string: "https://www.parentsimpliques.fr/wp-json/wp/v2/posts") else {
             print("Invalid URL")
             return
         }
-        
+
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
 
             // more code to come
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.articles
+            if let decodedResponse = try? JSONDecoder().decode([Post].self, from: data) {
+                results = decodedResponse
             }
         } catch {
             print("Invalid data")
@@ -49,15 +52,14 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct Response : Codable {
-    var articles: [Post]
+struct Post : Codable {
+        var id : Int
+//        var date : Date
+        var slug : String
+//        var link : URL
+        var title : Rendered
 }
 
-struct Post : Codable {
-    var id : Int
-    var date : Date
-    var guid : String
-    var slug : String
-    var link : URL
-    var title : String
+struct Rendered : Codable {
+    var rendered : String
 }
